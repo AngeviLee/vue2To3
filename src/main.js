f8f2425210c19d7f50577772cc80eb787bedb4b0
AngeviLee <568178364@qq.com>
2020-11-04 19:16:46 +0800

    安装引入vue的github上的插件包，为使用vue3.x的语法

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 安装引入vue的github上的插件包，为使用vue3.x的语法
+import VueCompositionAPI from '@vue/composition-api'
 
 Vue.config.productionTip = false
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
