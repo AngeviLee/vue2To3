e858f587a75c3613fe5afbc6f467df97a19debc8
AngeviLee <568178364@qq.com>
2020-11-05 11:04:46 +0800

    vue3.x方法2通过ref修改值

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,12 +33,15 @@ export default {
         const changeMsg = () => {
             // vue3.x 方式修改值
             state.msg2 = '你是一个大花旦!'
+            // vur3.x通过方式2ref修改值
+            msg3.value = '刚把得'
         }
 
         return {
             msg3,
             // vue3.x双向数据绑定
             ...toRefs(state),
+            // 返回函数方法
             changeMsg
         }
     }
