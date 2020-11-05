031954c0455b0c8edfb890c20c70027ab350b2f4
AngeviLee <568178364@qq.com>
2020-11-05 10:16:16 +0800

    vue3.x的第一种声明定义和vue2.x对比

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
+<div class="about">
     <h1>This is an about page</h1>
-  </div>
+    <p>{{ msg }}</p>
+    <p>{{ msg2 }}</p>
+</div>
 </template>
+
+<script>
+import {
+    reactive,
+    ref
+} from '@vue/composition-api'
+export default {
+    data() {
+        return {
+            msg: '对不起，我是一个警察'
+        }
+    },
+    // vue3的第一种方式
+    setup() {
+        const state = reactive({
+            msg2: '你好，我是坏蛋！'
+        })
+
+        return state
+    }
+}
+</script>
